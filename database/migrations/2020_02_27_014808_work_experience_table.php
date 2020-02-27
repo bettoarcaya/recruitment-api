0df0078c598b8841ef06f697bb312553f87277ac
Humberto Arcaya <betoarcaya@gmail.com>
2020-02-26 21:59:14 -0400

    working on the migrations

---
+++ database/migrations/2020_02_27_014808_work_experience_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class WorkExperienceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_experiences', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('time');
+            $table->string('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
